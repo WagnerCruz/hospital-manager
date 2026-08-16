d75a86bf14fdbaf70d91bbb34dcbcb03d133f098
wagnerfcruz <wagnerfcruz@gmail.com>
2026-08-16 19:00:01 -0300

    primeiro commit

---
+++ src/test/java/com/raidstack/hospital_manager/HospitalManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raidstack.hospital_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
