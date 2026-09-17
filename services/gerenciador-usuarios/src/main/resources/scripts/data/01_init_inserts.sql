5bc7306ea06f288787eeec1da886d1ab7771a7fe
AndrePina <pina.mendes@gmail.com>
2026-09-16 23:26:45 -0300

    Script para novos perfis e ajuste na validação do controller de cadastrar agendamento.

--- services/gerenciador-usuarios/src/main/resources/scripts/data/01_init_inserts.sql
+++ services/gerenciador-usuarios/src/main/resources/scripts/data/01_init_inserts.sql
@@ -30,7 +30,11 @@ VALUES
     (RANDOM_UUID(), 'ADMINISTRADOR_USUARIO_ATUALIZAR', 'Atualizar usuários'),
     (RANDOM_UUID(), 'ADMINISTRADOR_USUARIO_VISUALIZAR', 'Visualizar usuários'),
     (RANDOM_UUID(), 'ADMINISTRADOR_USUARIO_DESATIVAR', 'Desativar usuários'),
-    (RANDOM_UUID(), 'ADMINISTRADOR_USUARIO_GERENCIAR_PERFIL', 'Gerenciar perfis dos usuários');
+    (RANDOM_UUID(), 'ADMINISTRADOR_USUARIO_GERENCIAR_PERFIL', 'Gerenciar perfis dos usuários'),
+    (RANDOM_UUID(), 'AGENDAMENTO_CRIAR', 'Criar Agendamentos'),
+    (RANDOM_UUID(), 'AGENDAMENTO_VIZUALIZAR', 'Visualizar Agendamentos'),
+    (RANDOM_UUID(), 'AGENDAMENTO_ATUALIZAR', 'Atualizar Agendamentos');
+
 
 --- ASSOCIAR PERFIS AS PERMISSÕES
 INSERT INTO hospital.perfil_permissao (perfil_id, permissao_id)
@@ -44,3 +48,21 @@ SELECT perfil.id, permissao.id
 FROM hospital.perfis perfil
          JOIN hospital.permissoes permissao ON permissao.nome like 'USUARIO%'
 WHERE perfil.nome IN ('PACIENTE', 'MEDICO', 'ENFERMEIRO', 'ADMINISTRADOR');
+
+INSERT INTO hospital.perfil_permissao (perfil_id, permissao_id)
+SELECT perfil.id, permissao.id
+FROM hospital.perfis perfil
+         JOIN hospital.permissoes permissao ON permissao.nome IN ('AGENDAMENTO_CRIAR', 'AGENDAMENTO_VIZUALIZAR')
+WHERE perfil.nome IN ('ENFERMEIRO');
+
+INSERT INTO hospital.perfil_permissao (perfil_id, permissao_id)
+SELECT perfil.id, permissao.id
+FROM hospital.perfis perfil
+         JOIN hospital.permissoes permissao ON permissao.nome IN ('AGENDAMENTO_VIZUALIZAR', 'AGENDAMENTO_ATUALIZAR')
+WHERE perfil.nome IN ('MEDICO');
+
+INSERT INTO hospital.perfil_permissao (perfil_id, permissao_id)
+SELECT perfil.id, permissao.id
+FROM hospital.perfis perfil
+         JOIN hospital.permissoes permissao ON permissao.nome IN ('AGENDAMENTO_VIZUALIZAR')
+WHERE perfil.nome IN ('PACIENTE');
